79ac69a68a4a0e4c8dc0037340c725d573f8b9ea
Paul Wilson <paulanthonywilson@gmail.com>
2016-10-25 14:31:07 +0100

    adding the pubsubs

--- mix.exs
+++ mix.exs
@@ -10,24 +10,15 @@ defmodule PubsubSpike.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:gproc, :phoenix_pubsub, :logger],
      mod: {PubsubSpike, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:gproc, "~> 0.6.0"},
+      {:phoenix_pubsub, "~> 0.1"}
+    ]
   end
 end
